d886949421ede1f4e9bff6edbd03e949f1da89e0
OmarKh006 <khairyomar006@gmail.com>
2026-07-03 19:44:32 +0300

    project init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center align-middle w-full h-full">
+        <h1 className="text-3xl">Omar</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
