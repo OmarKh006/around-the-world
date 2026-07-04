7a925e0b6ef67305a1efc33b5e31dada412c2921
OmarKh006 <khairyomar006@gmail.com>
2026-07-04 18:33:41 +0300

    search input & region menu components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import Header from "./components/Header";
+import RegionMenu from "./components/RegionMenu";
+import SearchInput from "./components/SearchInput";
 
 function App() {
   return (
-    <div className="font-[Inter] dark:text-gray-100">
+    <div className="min-h-screen bg-gray-100 font-[Inter] dark:bg-gray-900 dark:text-gray-100">
       <Header />
+      <div className="container mx-auto px-5 md:px-0">
+        <div className="flex flex-col justify-between gap-10 md:h-14 md:flex-row md:gap-0">
+          <SearchInput />
+          <RegionMenu />
+        </div>
+      </div>
     </div>
   );
 }
