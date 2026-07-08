6b04e3abb58ef40560f771589c5d69ca559979c7
OmarKh006 <khairyomar006@gmail.com>
2026-07-08 14:45:39 +0300

    react router implementation

---
+++ src/pages/Country.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Country = () => {
+  const { country } = useParams();
+  return <div>{country}</div>;
+};
+
+export default Country;
